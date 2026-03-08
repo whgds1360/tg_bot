f6cf132b26294198dbc069d6b4ff954f7eafec69
Dr.Ghost64 <danilaburkov7@gmail.com>
2026-03-08 21:46:41 +0300

    Изменение названия кнопок

--- keyboard/keyboard.py
+++ keyboard/keyboard.py
@@ -3,8 +3,10 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def main_menu():
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Кнопка 1", callback_data="button1")],
-        [InlineKeyboardButton(text="Кнопка 2", callback_data="button2")]
+        [InlineKeyboardButton(text="Создать временную почту", callback_data="button1")],
+        [InlineKeyboardButton(text="Получить ключ", callback_data="button2")],
+        [InlineKeyboardButton(text="Настройки", callback_data="button3")],
+        [InlineKeyboardButton(text="Кинуть кубик", callback_data="button4")],
         ])
     return keyboard
 
